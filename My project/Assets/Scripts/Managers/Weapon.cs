b88b3e8912156bb0ccbe7113666f469faf0d545b
Laura Grimaldos <Lauravalentina_24@hotmail.com>
2024-08-27 00:18:14 -0500

    creacion de camara, generacíon mecanicas personaje principal (movimiento, shoot, rotacion)

---
+++ My project/Assets/Scripts/Managers/Weapon.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Windows;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject bulletprefab;
+    public Transform firePoint;
+    [SerializeField] private float _fireForce;
+
+
+
+    public void Fire()
+    {
+
+        GameObject bullet = Instantiate(bulletprefab, firePoint.position, firePoint.rotation);
+        bullet.GetComponent<Rigidbody>().AddForce(firePoint.up *  _fireForce, ForceMode.Impulse);
+    }
+
+
+}
