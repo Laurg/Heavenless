913f260618bd708004dbc19fe7fae68cebe245f6
NickArc <nickarc19@gmail.com>
2024-09-01 20:22:58 -0500

    Weapon implementation and boss fixes

--- MyProject/Assets/Scripts/Managers/EnemyStatsBoss.cs
+++ MyProject/Assets/Scripts/Managers/EnemyStatsBoss.cs
@@ -2,13 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyStats : Character_Stats
+public class EnemyStatsBoss : Character_Stats
 {
     [SerializeField] private float damage;
     [SerializeField] private float attackSpeed;
     [SerializeField] private bool canAttack;
-    [SerializeField] public GameObject EnemyType;
-    [SerializeField] public GameObject Boss;
 
     private void Start()
     {
@@ -33,27 +31,13 @@ public class EnemyStats : Character_Stats
 
     public override void InitVariables()
     {
-        
-        if (EnemyType == Boss)
-        {
-            maxHealth = 500;
-            SetHealthTo(maxHealth);
-            isDead = false;
+        maxHealth = 200;
+        SetHealthTo(maxHealth);
+        isDead = false;
 
-            damage = 10;
-            attackSpeed = 1.5f;
-            canAttack = true;
-        }
-        else
-        {
-            maxHealth = 30;
-            SetHealthTo(maxHealth);
-            isDead = false;
-
-            damage = 10;
-            attackSpeed = 1.5f;
-            canAttack = true;
-        }
+        damage = 10;
+        attackSpeed = 1.5f;
+        canAttack = true;
     }
 
 }
